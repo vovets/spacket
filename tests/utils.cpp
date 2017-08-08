6cdf11af6317eac45d21474e16e373068e276b20
Vladimir Krasovskiy <vovets@gmail.com>
2017-08-08 22:30:40 +0300

    json config for device configuration for tests

---
+++ tests/utils.cpp
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+#include <json.hpp>
+
+#include <fstream>
+
+namespace js = nlohmann;
+
+PortConfig fromJson(const std::string& path) {
+    std::ifstream i(path);
+    js::json j;
+    i >> j;
+    return PortConfig{j.at("device")};
+}
---
+++ tests/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <spacket/result.h>
+#include <spacket/config.h>
+
+PortConfig fromJson(const std::string& path);
