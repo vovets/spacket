560f157d461e21e44765806fb05822ee7e2f9d84
Vladimir Krasovskiy <vovets@gmail.com>
2017-12-14 04:58:09 +0300

    get rid of idle thread, class template for RTT stream

--- test_chibios_blinker/main.cpp
+++ test_chibios_blinker/main.cpp
@@ -14,70 +14,132 @@
     limitations under the License.
 */
 
-#include <stdio.h>
-#include <string.h>
-
 #include "ch.h"
 #include "hal.h"
 #include "ch_test.h"
 #include "shell.h"
 #include "chprintf.h"
 #include "hal_serial.h"
+#include "hal_channels.h"
 #include "SEGGER_RTT.h"
 
-#define _rtt_stream_data                                                   \
-  _base_sequential_stream_data
+#include <stdio.h>
+#include <string.h>
 
-struct RTTStreamVMT {
-  _base_sequential_stream_methods
+#include <limits>
+
+static inline systime_t systimeDiff(systime_t currentTime, systime_t prevTime) {
+    return currentTime >= prevTime
+        ? currentTime - prevTime
+        : std::numeric_limits<systime_t>::max() - (prevTime - currentTime);
+}
+
+template <unsigned BUFFER_INDEX, uint32_t READ_POLL_PERIOD_MS>
+class RTTStreamT: public BaseSequentialStream, public BaseChannel {
+public:
+    RTTStreamT()
+        : BaseSequentialStream{(BaseSequentialStreamVMT*)&vmt_}
+        , BaseChannel{&vmt_}
+    {
+    }
+
+private:
+    static RTTStreamT* cast(void* p) { return static_cast<RTTStreamT*>(p); }
+    
+private:
+    static size_t write(void *ip, const uint8_t *bp, size_t n) { return cast(ip)->write(bp, n); }
+    static size_t read(void *ip, uint8_t *bp, size_t n) { return cast(ip)->read(bp, n); }
+    static msg_t put(void *ip, uint8_t b) { return cast(ip)->put(b); }
+    static msg_t get(void *ip) { return cast(ip)->get(); }
+    static size_t writet(
+        void *ip,
+        const uint8_t *bp,
+        size_t n,
+        systime_t time) {
+        return cast(ip)->writet(bp, n, time);
+    }
+    static size_t readt(void *ip, uint8_t *bp, size_t n, systime_t time) {
+        return cast(ip)->readt(bp, n, time);
+    }
+    static msg_t putt(void *ip, uint8_t b, systime_t time) { return cast(ip)->putt(b, time); }
+    static msg_t gett(void *ip, systime_t time) { return cast(ip)->gett(time); }
+
+private:
+    size_t writet(const uint8_t *bp, size_t n, systime_t) {
+        return SEGGER_RTT_Write(BUFFER_INDEX, bp, n);
+    }
+
+    size_t readt(uint8_t *bp, size_t n, systime_t timeout) {
+        systime_t prevTime = osalOsGetSystemTimeX();
+        size_t read = 0;
+        while(true) {
+            read = SEGGER_RTT_Read(BUFFER_INDEX, bp, n);
+            if (read) {
+                return read;
+            }
+            chThdSleepMilliseconds(READ_POLL_PERIOD_MS);
+            if (timeout != TIME_INFINITE) {
+                systime_t currentTime = osalOsGetSystemTimeX();
+                systime_t diff = systimeDiff(currentTime, prevTime);
+                if (diff > timeout) {
+                    return read;
+                }
+                timeout -= diff;
+                prevTime = currentTime;
+            }
+        }
+    }
+
+    msg_t putt(uint8_t b, systime_t time) {
+        switch (writet(&b, 1, time)) {
+            case 1: return MSG_OK;
+            default: return MSG_RESET;
+        }
+    }
+    
+    msg_t gett(systime_t time) {
+        uint8_t b;
+        switch (readt(&b, 1, time)) {
+            case 1: return b;
+            default: return MSG_RESET;
+        }
+    }
+
+    msg_t put(uint8_t b) {
+        return putt(b, TIME_INFINITE);
+    }
+
+    msg_t get() {
+        return gett(TIME_INFINITE);
+    }
+    
+    size_t write(const uint8_t *bp, size_t n) {
+        return writet(bp, n, TIME_INFINITE);
+    }
+
+    size_t read(uint8_t *bp, size_t n) {
+        return readt(bp, n, TIME_INFINITE);
+    }
+    
+private:
+    static const BaseChannelVMT vmt_;
 };
 
-typedef struct {
-    const struct RTTStreamVMT *vmt;
-    _rtt_stream_data
-} RTTStream;
+template <unsigned BUFFER_INDEX, uint32_t READ_POLL_PERIOD_MS>
+const BaseChannelVMT RTTStreamT<BUFFER_INDEX, READ_POLL_PERIOD_MS>::vmt_ = {
+    RTTStreamT::write,
+    RTTStreamT::read,
+    RTTStreamT::put,
+    RTTStreamT::get,
+    RTTStreamT::putt,
+    RTTStreamT::gett,
+    RTTStreamT::writet,
+    RTTStreamT::readt
+};
 
-static size_t rtt_write(void *ip, const uint8_t *bp, size_t n) {
-    (void)ip;
-    return SEGGER_RTT_Write(0, bp, n);
-}
+using RTTStream = RTTStreamT<0, 10>;
 
-static size_t rtt_read(void *ip, uint8_t *bp, size_t n) {
-    (void)ip;
-    return SEGGER_RTT_Read(0, bp, n);
-}
-
-static msg_t rtt_put(void *ip, uint8_t b) {
-    if (rtt_write(ip, &b, 1) != 1) {
-        return MSG_RESET;
-    }
-    return MSG_OK;
-}
-
-static msg_t rtt_get(void *ip) {
-    uint8_t b;
-    if (rtt_read(ip, &b, 1) != 1) {
-        return MSG_RESET;
-    }
-    return b;
-}
-
-static const struct RTTStreamVMT rtt_vmt = {rtt_write, rtt_read, rtt_put, rtt_get};
-
-static RTTStream rttStream_;
-
-static BaseSequentialStream* const rttStream = (BaseSequentialStream*)&rttStream_;
-
-static void RTTStreamObjectInit(RTTStream *s) {
-    s->vmt = &rtt_vmt;
-}
-
-
-/*===========================================================================*/
-/* Command line related.                                                     */
-/*===========================================================================*/
-
-#define TEST_WA_SIZE    THD_WORKING_AREA_SIZE(256)
+static RTTStream rttStream{};
 
 static void cmd_write(BaseSequentialStream *chp, int argc, char *argv[]) {
   static uint8_t buf[] =
@@ -104,10 +166,7 @@ static void cmd_write(BaseSequentialStream *chp, int argc, char *argv[]) {
     return;
   }
 
-  while (chnGetTimeout((BaseChannel *)chp, TIME_IMMEDIATE) == Q_TIMEOUT) {
-    streamWrite(&SD1, buf, sizeof buf - 1);
-  }
-  chprintf(chp, "\r\n\nstopped\r\n");
+  streamWrite(&SD1, buf, sizeof buf - 1);
 }
 
 static const ShellCommand commands[] = {
@@ -116,28 +175,23 @@ static const ShellCommand commands[] = {
 };
 
 static const ShellConfig shell_cfg1 = {
-  (BaseSequentialStream *)&SD1,
+  (BaseSequentialStream *)&rttStream,
   commands
 };
 
-/*===========================================================================*/
-/* Generic code.                                                             */
-/*===========================================================================*/
 
 /*
  * Blinker thread, times are in milliseconds.
  */
-static THD_WORKING_AREA(waThread1, 160);
+static THD_WORKING_AREA(waThread1, 176);
 static __attribute__((noreturn)) THD_FUNCTION(Thread1, arg) {
   (void)arg;
   chRegSetThreadName("blinker");
   while (true) {
     const systime_t time = 250;
     palClearPad(GPIOC, GPIOC_LED);
-    chprintf(rttStream, "flip O\n\r");
     chThdSleepMilliseconds(time);
     palSetPad(GPIOC, GPIOC_LED);
-    chprintf(rttStream, "flop *\n\r");
     chThdSleepMilliseconds(time);
   }
 }
@@ -158,9 +212,7 @@ int __attribute__((noreturn)) main(void) {
   halInit();
   chSysInit();
 
-  RTTStreamObjectInit(&rttStream_);
-
-  chprintf(rttStream, "Hello from RTT!\n\r");
+  chprintf(&rttStream, "RTT ready\n\r");
   
   /*
    * Initializes a serial driver.
@@ -180,11 +232,8 @@ int __attribute__((noreturn)) main(void) {
   chThdCreateStatic(
       waShellThread, sizeof(waShellThread), NORMALPRIO, shellThread, (void*)&shell_cfg1);
 
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state.
-   */
   while (true) {
-    chThdSleepMilliseconds(1000);
+    port_wait_for_interrupt();
+    CH_CFG_IDLE_LOOP_HOOK();
   }
 }
