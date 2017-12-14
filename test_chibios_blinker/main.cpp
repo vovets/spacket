aa5d6ab7ef442d00710ed07af76ce4096ad5b29b
Vladimir Krasovskiy <vovets@gmail.com>
2017-12-14 06:55:49 +0300

    cxx thread wrapper, writer thread, start/stop_write commands

--- test_chibios_blinker/main.cpp
+++ test_chibios_blinker/main.cpp
@@ -137,70 +137,116 @@ const BaseChannelVMT RTTStreamT<BUFFER_INDEX, READ_POLL_PERIOD_MS>::vmt_ = {
     RTTStreamT::readt
 };
 
+template <size_t STACK_SIZE>
+class StaticThread {
+public:
+    thread_t* create(tprio_t prio, tfunc_t f, void* arg) {
+        return chThdCreateStatic(
+            workingArea,
+            sizeof(workingArea),
+            prio,
+            f,
+            arg);
+    }
+
+private:
+    THD_WORKING_AREA(workingArea, STACK_SIZE);
+};
+
 using RTTStream = RTTStreamT<0, 10>;
 
-static RTTStream rttStream{};
+RTTStream rttStream{};
+StaticThread<176> blinkerThread{};
+StaticThread<128> writerThread{};
+thread_t* writerThreadPtr = nullptr;
+StaticThread<2048> shellThread_{};
 
-static void cmd_write(BaseSequentialStream *chp, int argc, char *argv[]) {
-  static uint8_t buf[] =
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+static void writerThreadFunction(void*);
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: write\r\n");
-    return;
-  }
+static void cmd_start_write(BaseSequentialStream *chp, int argc, char *argv[]) {
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: start_write\r\n");
+        return;
+    }
+    if(writerThreadPtr) {
+        chprintf(chp, "already running\r\n");
+        return;
+    }
+    writerThreadPtr = writerThread.create(NORMALPRIO, writerThreadFunction, 0);
+    chprintf(chp, "started\r\n");
+}
 
-  streamWrite(&SD1, buf, sizeof buf - 1);
+static void cmd_stop_write(BaseSequentialStream *chp, int argc, char *argv[]) {
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: stop_write\r\n");
+        return;
+    }
+    if (!writerThreadPtr) {
+        chprintf(chp, "not running\r\n");
+        return;
+    }
+    chThdTerminate(writerThreadPtr);
+    chThdWait(writerThreadPtr);
+    writerThreadPtr = nullptr;
+    chprintf(chp, "stopped\r\n");
 }
 
 static const ShellCommand commands[] = {
-  {"write", cmd_write},
-  {NULL, NULL}
+    {"start_write", cmd_start_write},
+    {"stop_write", cmd_stop_write},
+    {NULL, NULL}
 };
 
-static const ShellConfig shell_cfg1 = {
-  (BaseSequentialStream *)&rttStream,
-  commands
+static const ShellConfig shellCfg = {
+    &rttStream,
+    commands
 };
 
 
-/*
- * Blinker thread, times are in milliseconds.
- */
-static THD_WORKING_AREA(waThread1, 176);
-static __attribute__((noreturn)) THD_FUNCTION(Thread1, arg) {
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (true) {
-    const systime_t time = 250;
-    palClearPad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(time);
-    palSetPad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(time);
-  }
+
+static __attribute__((noreturn)) THD_FUNCTION(blinkerThreadFunction, arg) {
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while(true) {
+        const systime_t time = 250;
+        palClearPad(GPIOC, GPIOC_LED);
+        chThdSleepMilliseconds(time);
+        palSetPad(GPIOC, GPIOC_LED);
+        chThdSleepMilliseconds(time);
+    }
 }
 
-static THD_WORKING_AREA(waShellThread, 2048);
+static THD_FUNCTION(writerThreadFunction, arg) {
+    static uint8_t buf[] =
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
 
-/*
- * Application entry point.
- */
+    (void)arg;
+
+    chRegSetThreadName("writer");
+
+    while(!chThdShouldTerminateX()) {
+        streamWrite(&SD1, buf, sizeof buf - 1);
+        chThdYield();
+    }
+}
+
 int __attribute__((noreturn)) main(void) {
   /*
    * System initializations.
@@ -214,23 +260,12 @@ int __attribute__((noreturn)) main(void) {
 
   chprintf(&rttStream, "RTT ready\n\r");
   
-  /*
-   * Initializes a serial driver.
-   */
   sdStart(&SD1, NULL);
 
-  /*
-   * Shell manager initialization.
-   */
   shellInit();
 
-  /*
-   * Creates the blinker thread.
-   */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-
-  chThdCreateStatic(
-      waShellThread, sizeof(waShellThread), NORMALPRIO, shellThread, (void*)&shell_cfg1);
+  blinkerThread.create(NORMALPRIO, blinkerThreadFunction, 0);
+  shellThread_.create(NORMALPRIO, shellThread, const_cast<ShellConfig*>(&shellCfg));
 
   while (true) {
     port_wait_for_interrupt();
