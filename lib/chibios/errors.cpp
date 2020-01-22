9fab9d8f14e0f2c366d4daa56552fce00dea99aa
Vladimir Krasovskiy <vovets@gmail.com>
2020-01-22 17:52:38 +0300

    fixed: toString(Error) did not terminate result with 0 byte (chprintf does not do it)

--- lib/chibios/errors.cpp
+++ lib/chibios/errors.cpp
@@ -8,6 +8,7 @@
 const char* toString(Error e, char* buffer, std::size_t size) {
     BufferView bv{reinterpret_cast<std::uint8_t*>(buffer), size};
     BufferSequentialStreamT<BufferView> ss{bv};
-    chprintf(&ss, "%d.%d", e.source, e.code);
+    int n = chprintf(&ss, "%d.%d", e.source, e.code);
+    buffer[std::min(size - 1, static_cast<std::size_t>(std::max(0, n)))] = 0;
     return buffer;
 }
